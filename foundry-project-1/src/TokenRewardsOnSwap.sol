9b2523810355f5fa7634a9c3ac205ce056c24672
Vatsal Sanchala <bevatsal1122@gmail.com>
2024-09-21 16:57:48 +0800

    feat: ads token reward contract

---
+++ foundry-project-1/src/TokenRewardsOnSwap.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "@uniswap/v4-core/src/types/BeforeSwapDelta.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+struct PoolConfig {
+    address tokenAddress;
+    address owner;
+    address rewardsTokenStockAddress;
+    int24 minimumQualificationAmount;
+    int24 rewardTokenAmount;
+}
+
+contract TokenGated is BaseHook {
+    using PoolIdLibrary for PoolKey;
+
+    mapping(PoolId => PoolConfig) public pools;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: true,
+                beforeAddLiquidity: false,
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: false,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+    function afterInitialize(
+        address sender,
+        PoolKey calldata key,
+        uint160,
+        int24,
+        bytes calldata hookData
+    ) external override returns (bytes4) {
+        (address _tokenAddress, address _rewardsTokenStockAddress, int24 _minimumQualificationAmount, int24 _rewardTokenAmount) = abi.decode(hookData, (address, address, int24, int24));
+        PoolConfig memory pool = PoolConfig({
+            owner: sender,
+            rewardsTokenStockAddress: _rewardsTokenStockAddress,
+            minimumQualificationAmount: _minimumQualificationAmount,
+            rewardTokenAmount: _rewardTokenAmount,
+            tokenAddress: _tokenAddress
+        });
+        PoolId poolId = key.toId();
+        pools[poolId] = pool;
+        return BaseHook.afterInitialize.selector;
+    }
+
+    function setPoolConfig(
+        PoolKey calldata key,
+        address _tokenAddress,
+        address __rewardsTokenStockAddress,
+        int24 _minimumQualificationAmount,
+        int24 _rewardTokenAmount 
+    ) external {
+        PoolId poolId = key.toId();
+        pools[poolId].tokenAddress = _tokenAddress;
+        pools[poolId].rewardsTokenStockAddress = __rewardsTokenStockAddress;
+        pools[poolId].minimumQualificationAmount =_minimumQualificationAmount;
+        pools[poolId].rewardTokenAmount = _rewardTokenAmount;
+    }
+
+    function afterSwap(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) external override returns (bytes4, int128) {
+        PoolId poolId = key.toId();
+        PoolConfig memory pool = pools[poolId];
+        IERC20 token = IERC20(pool.tokenAddress);
+        uint256 senderBalance = token.balanceOf(sender);
+
+        require(
+            senderBalance > 0,
+            "Swap denied: sender has zero token balance"
+        );
+
+        return (BaseHook.afterSwap.selector, pool.rewardTokenAmount);
+    }
+}
