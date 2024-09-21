1781f747c7293e211bbe77041e7271ecb1d9d100
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2024-09-21 15:32:21 +0800

    chore: revert mappings, fix univ router

--- foundry-project-1/src/Constants.sol
+++ foundry-project-1/src/Constants.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import './IUniversalRouter.sol';
 
 contract Constants {
-    address public constant UNIVERSAL_ROUTER = "0x927038542746c06F1b2F7F550a3c90AEBdDa4E85"; // On "Base Sepolia"
+    address public constant UNIVERSAL_ROUTER = 0x927038542746c06F1b2F7F550a3c90AEBdDa4E85; // On "Base Sepolia"
 
     function getMsgSender(address _router) external view returns (address) {
         return IUniversalRouter(_router).msgSender();
