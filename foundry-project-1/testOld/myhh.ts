07e6d3f49e2c96c2f7c5b52f0ce3f3d1d0f42611
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2024-09-21 14:32:08 +0800

    feat: running test in hardhat

---
+++ foundry-project-1/testOld/myhh.ts
@@ -0,0 +1,16 @@
+import { expect } from "chai";
+import hre, { ethers } from "hardhat";
+
+describe("Counter", function () {
+  it("Should set the right unlockTime", async function () {
+    // deploy a lock contract where funds can be withdrawn
+    // one year in the future
+
+    const lock = await hre.ethers.deployContract("Counter", [], {
+      value: 0,
+    });
+
+    // assert that the value is correct
+    expect(await lock.beforeSwapCount()).to.equal(ethers.toBigInt(0));
+  });
+});
