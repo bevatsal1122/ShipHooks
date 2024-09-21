2d3f46bc55ee711a7b59748660e54fb46f1e588e
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2024-09-21 15:21:58 +0800

    fix: constants contract

--- foundry-project-1/src/Constants.sol
+++ foundry-project-1/src/Constants.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import './IUniversalRouter';
+import './IUniversalRouter.sol';
 
 contract Constants {
-    constant address UNIVERSAL_ROUTER = "0x927038542746c06F1b2F7F550a3c90AEBdDa4E85" // On "Base Sepolia"
+    address public constant UNIVERSAL_ROUTER = "0x927038542746c06F1b2F7F550a3c90AEBdDa4E85"; // On "Base Sepolia"
 
     function getMsgSender(address _router) external view returns (address) {
         return IUniversalRouter(_router).msgSender();
