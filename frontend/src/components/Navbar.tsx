dbbd02ee922d4dc93c0cc895127a92c922845226
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2024-09-22 04:45:06 +0800

    chore: nav link to docs when available

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import ConnectButton from "@/lib/ConnectWallet";
+import { isExternal } from "util/types";
 
 const Navbar = () => {
   const router = useRouter();
@@ -11,6 +12,7 @@ const Navbar = () => {
     { name: "Home", path: "/" },
     { name: "Create", path: "/create" },
     { name: "Swap", path: "/swap" },
+    { name: "Docs", path: "/", isExternal: true },
   ];
 
   return (
@@ -19,8 +21,15 @@ const Navbar = () => {
         <span className="text-xl font-bold mr-8">ShipHooks 🚀</span>
         <div className="space-x-4">
           {navItems.map((item) => (
-            <Link className="cursor-pointer" key={item.name} href={item.path}>
-              <Button className="text-white hover:text-gray-300 cursor-pointer transition-colors">{item.name}</Button>
+            <Link
+              className="cursor-pointer"
+              key={item.name}
+              href={item.path}
+              target={item.isExternal ? "_blank" : ""}
+            >
+              <Button className="text-white hover:text-gray-300 cursor-pointer transition-colors">
+                {item.name}
+              </Button>
             </Link>
           ))}
         </div>
