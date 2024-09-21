7e48819651a675ffac2477a5a4791f5d56f76952
Raghav Jindal <raghavjindal0212@gmail.com>
2024-09-22 00:06:40 +0800

    added landing page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,28 +1,52 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import "@rainbow-me/rainbowkit/styles.css";
-
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { WagmiProvider } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, base, sepolia } from "wagmi/chains";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { mainnet, arbitrum, sepolia } from "@reown/appkit/networks";
+import { Config, cookieStorage, createStorage, http } from "@wagmi/core";
+import { createAppKit } from "@reown/appkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const config = getDefaultConfig({
-  appName: "shiphooks",
-  projectId: "a7a48a6002426e8eaafe354ae0bb212b",
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+export const projectId = "a7a48a6002426e8eaafe354ae0bb212b";
+// Set up queryClient
+const queryClient = new QueryClient();
+
+export const metadata = {
+  name: "shiphooks",
+  description: "openzeppelin for uniswap hooks",
+  url: "https://example.com", // origin must match your domain and subdomain
+  icons: ["https://example.com/favicon.png"],
+};
+
+export const networks = [mainnet, arbitrum, sepolia];
+
+// Create wagmiAdapter
+export const wagmiAdapter = new WagmiAdapter({
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  ssr: true,
+  projectId,
+  networks,
 });
 
-const queryClient = new QueryClient();
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks: [mainnet, arbitrum, sepolia],
+  defaultNetwork: mainnet,
+  metadata: metadata,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={wagmiAdapter.wagmiConfig as Config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          {" "}
-          <Component {...pageProps} />
-        </RainbowKitProvider>
+        {" "}
+        <Component {...pageProps} />
       </QueryClientProvider>
     </WagmiProvider>
   );
